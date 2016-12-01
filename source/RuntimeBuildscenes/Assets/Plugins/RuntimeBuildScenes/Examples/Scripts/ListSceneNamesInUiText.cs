4494c52815d101520203f6306eb8a0b8a9d560fd
Sindri Jóelsson <sindrijo@gmail.com>
2016-12-01 03:20:28 +0100

    Add examples

---
+++ source/RuntimeBuildscenes/Assets/Plugins/RuntimeBuildScenes/Examples/Scripts/ListSceneNamesInUiText.cs
@@ -0,0 +1,18 @@
+﻿namespace RuntimeBuildscenes
+{
+    using UnityEngine ;
+
+    using System.Collections;
+    using System.Linq;
+    using UnityEngine.UI;
+
+    public class ListSceneNamesInUiText : MonoBehaviour
+    {
+        private void Awake()
+        {
+            var textComponent = GetComponent<Text>();
+            textComponent.text = new string(BuildScenes.Records.SelectMany(sr => sr.ToString() + "\n").ToArray());
+        }
+    }
+
+}
